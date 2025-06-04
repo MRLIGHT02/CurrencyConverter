ef63d42c32d5c5d2e8a5403a841b834e67e0274b
MRLIGHT02 <saifali42020@gmail.com>
2025-06-04 15:30:13 +0530

    Add project files.

---
+++ CurrencyConverter/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CurrencyConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
